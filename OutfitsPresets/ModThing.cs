a75e6d228f87790a9f025e68f16a0ec2de21998b
Viper <momo2010sofia@gmail.com>
2025-03-15 21:09:24 +0000

    Imposter wins again

---
+++ OutfitsPresets/ModThing.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[HarmonyPatch(typeof(ModManager), nameof(ModManager.LateUpdate))]
+class ModManagerLateUpdatePatch
+{
+    public static void Prefix(ModManager __instance)
+    {
+        __instance.ShowModStamp(); // required ok!
+    }
+}
